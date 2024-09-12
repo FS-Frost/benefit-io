87010e1560970d3b302b508709d8b4583ac38013
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2024-09-12 19:47:04 -0300

    Filtros.

--- src/lib/gui/pages/ExplorarBeneficios.svelte
+++ src/lib/gui/pages/ExplorarBeneficios.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
     import text from "$lib/text";
     import { onMount } from "svelte";
-    import { Oferta, Producto, type Proveedor } from "$lib/beneficios";
+    import { Descuento, Oferta, Producto } from "$lib/beneficios";
     import { data } from "$lib/data";
     import { z } from "zod";
 
-    let dias: InfoDia[] = [];
+    let infoDias: InfoDia[] = [];
 
-    let proveedores: Proveedor[] = [];
+    let proveedores: string[] = [];
+
+    let productos: string[] = [];
+
+    let dias: string[] = [];
+
+    let tiendas: string[] = [];
 
     let proveedorSeleccionado: string = "";
+
+    let productoSeleccionado: string = "";
+
+    let diaSeleccionado: string = "";
+
+    let tiendaSeleccionada: string = "";
 
     let filtrosVisibles: boolean = false;
 
@@ -27,7 +39,111 @@
 
     type InfoDia = z.infer<typeof InfoDia>;
 
-    onMount(() => {
+    function filtrarProveedores(): void {
+        console.log("filtrarProveedores");
+        proveedores = [];
+        productos = [];
+        dias = [];
+        tiendas = [];
+        proveedorSeleccionado = "";
+        productoSeleccionado = "";
+        diaSeleccionado = "";
+        tiendaSeleccionada = "";
+
+        for (const descuento of data) {
+            if (!proveedores.includes(descuento.proveedor)) {
+                proveedores.push(descuento.proveedor);
+            }
+        }
+
+        proveedores = [...proveedores];
+    }
+
+    function filtrarProductos(): void {
+        console.log("filtrarProductos");
+        diaSeleccionado = "";
+        tiendaSeleccionada = "";
+        productos = [];
+        dias = [];
+        tiendas = [];
+
+        for (const descuento of data) {
+            if (proveedorSeleccionado.length > 0) {
+                if (descuento.proveedor !== proveedorSeleccionado) {
+                    continue;
+                }
+            }
+
+            if (!productos.includes(descuento.producto)) {
+                productos.push(descuento.producto);
+            }
+        }
+
+        productos = [...productos];
+    }
+
+    function filtrarDias(): void {
+        console.log("filtrarDias");
+        tiendaSeleccionada = "";
+        dias = [];
+        tiendas = [];
+
+        for (const descuento of data) {
+            if (proveedorSeleccionado.length > 0) {
+                if (descuento.proveedor !== proveedorSeleccionado) {
+                    continue;
+                }
+            }
+
+            if (productoSeleccionado.length > 0) {
+                if (descuento.producto !== productoSeleccionado) {
+                    continue;
+                }
+            }
+
+            if (!dias.includes(descuento.dia)) {
+                dias.push(descuento.dia);
+            }
+        }
+
+        dias = [...dias];
+    }
+
+    function filtrarTiendas(): void {
+        console.log("filtrarTiendas");
+        tiendas = [];
+
+        for (const descuento of data) {
+            if (proveedorSeleccionado.length > 0) {
+                if (descuento.proveedor !== proveedorSeleccionado) {
+                    continue;
+                }
+            }
+
+            if (productoSeleccionado.length > 0) {
+                if (descuento.producto !== productoSeleccionado) {
+                    continue;
+                }
+            }
+
+            if (diaSeleccionado.length > 0) {
+                if (descuento.dia !== diaSeleccionado) {
+                    continue;
+                }
+            }
+
+            if (!tiendas.includes(descuento.tienda)) {
+                tiendas.push(descuento.tienda);
+            }
+        }
+
+        tiendas = [...tiendas];
+    }
+
+    function filtrarDescuentos(descuentos: Descuento[]): void {
+        console.log("filtrarDescuentos");
+        infoDias = [];
+
         const nombresDias: string[] = [
             "lunes",
             "martes",
@@ -38,50 +154,80 @@
             "domingo",
         ];
 
-        for (const descuento of data) {
+        for (const descuento of descuentos) {
             if (descuento.dia.split(" ").length > 1) {
                 continue;
             }
 
-            let indexDia = dias.findIndex((x) => x.dia === descuento.dia);
+            if (
+                proveedorSeleccionado.length > 0 &&
+                descuento.proveedor !== proveedorSeleccionado
+            ) {
+                continue;
+            }
+
+            if (
+                productoSeleccionado.length > 0 &&
+                descuento.producto !== productoSeleccionado
+            ) {
+                continue;
+            }
+
+            if (
+                diaSeleccionado.length > 0 &&
+                descuento.dia !== diaSeleccionado
+            ) {
+                continue;
+            }
+
+            if (
+                tiendaSeleccionada.length > 0 &&
+                descuento.tienda !== tiendaSeleccionada
+            ) {
+                continue;
+            }
+
+            let indexDia = infoDias.findIndex((x) => x.dia === descuento.dia);
 
             if (indexDia < 0) {
-                dias.push({
+                infoDias.push({
                     dia: descuento.dia,
                     productos: [],
                     orden: nombresDias.indexOf(descuento.dia.toLowerCase()),
                 });
 
-                indexDia = dias.length - 1;
+                indexDia = infoDias.length - 1;
             }
 
-            let indexProducto = dias[indexDia].productos.findIndex(
+            let indexProducto = infoDias[indexDia].productos.findIndex(
                 (x) => x.nombre === descuento.producto,
             );
 
             if (indexProducto < 0) {
-                dias[indexDia].productos.push({
+                infoDias[indexDia].productos.push({
                     nombre: descuento.producto,
                     proveedor: descuento.proveedor,
                     ofertas: [],
                 });
 
-                indexProducto = dias[indexDia].productos.length - 1;
+                indexProducto = infoDias[indexDia].productos.length - 1;
             }
 
-            let indexOferta = dias[indexDia].productos[
+            let indexOferta = infoDias[indexDia].productos[
                 indexProducto
             ].ofertas.findIndex((x) => x.tienda === descuento.tienda);
 
             if (indexOferta < 0) {
-                dias[indexDia].productos[indexProducto].ofertas.push({
+                infoDias[indexDia].productos[indexProducto].ofertas.push({
                     tienda: descuento.tienda,
                     descuento: descuento.descuento,
                 });
             }
         }
 
-        dias.sort((a, b) => {
+        proveedores = [...proveedores];
+
+        infoDias.sort((a, b) => {
             if (a.orden > b.orden) {
                 return 1;
             }
@@ -93,9 +239,17 @@
             return 0;
         });
 
-        dias = [...dias];
+        infoDias = [...infoDias];
 
-        console.log(dias);
+        console.log(infoDias);
+    }
+
+    onMount(() => {
+        filtrarDescuentos(data);
+        filtrarProveedores();
+        filtrarProductos();
+        filtrarDias();
+        filtrarTiendas();
     });
 </script>
 
@@ -122,10 +276,18 @@
             <div class="field">
                 <label class="label" for="">Institución</label>
                 <div class="select">
-                    <select bind:value={proveedorSeleccionado}>
+                    <select
+                        bind:value={proveedorSeleccionado}
+                        on:change={() => {
+                            filtrarProductos();
+                            filtrarDescuentos(data);
+                        }}
+                    >
+                        <option value="">Ninguna</option>
+
                         {#each proveedores as proveedor}
-                            <option value={proveedor.nombre}>
-                                {proveedor.nombre}
+                            <option value={proveedor}>
+                                {proveedor}
                             </option>
                         {/each}
                     </select>
@@ -135,10 +297,18 @@
             <div class="field">
                 <label class="label" for="">Producto</label>
                 <div class="select">
-                    <select bind:value={proveedorSeleccionado}>
-                        {#each proveedores as proveedor}
-                            <option value={proveedor.nombre}>
-                                {proveedor.nombre}
+                    <select
+                        bind:value={productoSeleccionado}
+                        on:change={() => {
+                            filtrarDias();
+                            filtrarDescuentos(data);
+                        }}
+                    >
+                        <option value="">Ninguno</option>
+
+                        {#each productos as producto}
+                            <option value={producto}>
+                                {producto}
                             </option>
                         {/each}
                     </select>
@@ -148,10 +318,18 @@
             <div class="field">
                 <label class="label" for="">Día de la semana</label>
                 <div class="select">
-                    <select bind:value={proveedorSeleccionado}>
-                        {#each proveedores as proveedor}
-                            <option value={proveedor.nombre}>
-                                {proveedor.nombre}
+                    <select
+                        bind:value={diaSeleccionado}
+                        on:change={() => {
+                            filtrarTiendas();
+                            filtrarDescuentos(data);
+                        }}
+                    >
+                        <option value="">Ninguno</option>
+
+                        {#each dias as dia}
+                            <option value={dia}>
+                                {dia}
                             </option>
                         {/each}
                     </select>
@@ -161,10 +339,17 @@
             <div class="field">
                 <label class="label" for="">Local</label>
                 <div class="select">
-                    <select bind:value={proveedorSeleccionado}>
-                        {#each proveedores as proveedor}
-                            <option value={proveedor.nombre}>
-                                {proveedor.nombre}
+                    <select
+                        bind:value={tiendaSeleccionada}
+                        on:change={() => {
+                            filtrarDescuentos(data);
+                        }}
+                    >
+                        <option value="">Ninguno</option>
+
+                        {#each tiendas as tienda}
+                            <option value={tienda}>
+                                {tienda}
                             </option>
                         {/each}
                     </select>
@@ -174,7 +359,7 @@
     {/if}
 
     <div class="descuentos mt-4">
-        {#each dias as dia}
+        {#each infoDias as dia}
             <div class="dia">
                 <div class="subtitulo">Septiembre: {dia.dia}</div>
 
