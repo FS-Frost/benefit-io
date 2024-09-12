0e6e87a1376aac3240a9da37476ca7545a0b106a
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2024-09-12 20:32:52 -0300

    Sesión.

--- src/lib/gui/pages/Inicio.svelte
+++ src/lib/gui/pages/Inicio.svelte
@@ -2,11 +2,129 @@
     import text, { capitalizeFirstLetter } from "$lib/text";
     import { onMount } from "svelte";
     import VistaBeneficio from "../VistaBeneficio.svelte";
-    import type { Beneficio } from "$lib/beneficios";
+    import {
+        Oferta,
+        Producto,
+        type Beneficio,
+        type Descuento,
+    } from "$lib/beneficios";
+    import { data } from "$lib/data";
+    import { z } from "zod";
+    import { KEY_PRODUCTOS } from "$lib/session";
+
+    let infoDias: InfoDia[] = [];
 
     let fecha: string = "";
 
-    let beneficios: Beneficio[] = [];
+    let productosUsuario: Producto[] = [];
+
+    const InfoProducto = Producto.extend({
+        ofertas: Oferta.array(),
+    });
+
+    type InfoProducto = z.infer<typeof InfoProducto>;
+
+    const InfoDia = z.object({
+        dia: z.string(),
+        orden: z.number(),
+        productos: InfoProducto.array(),
+    });
+
+    type InfoDia = z.infer<typeof InfoDia>;
+
+    function filtrarDescuentos(descuentos: Descuento[]): void {
+        console.log("filtrarDescuentos");
+        infoDias = [];
+
+        const hoy = capitalizeFirstLetter(
+            new Date().toLocaleDateString("es-ES", {
+                weekday: "long",
+            }),
+        ).toLowerCase();
+
+        const nombresDias: string[] = [
+            "lunes",
+            "martes",
+            "miércoles",
+            "jueves",
+            "viernes",
+            "sábado",
+            "domingo",
+        ];
+
+        for (const descuento of descuentos) {
+            if (descuento.dia.split(" ").length > 1) {
+                continue;
+            }
+
+            if (descuento.dia.toLowerCase() !== hoy) {
+                continue;
+            }
+
+            if (
+                !productosUsuario.some(
+                    (x) =>
+                        x.proveedor === descuento.proveedor &&
+                        x.nombre === descuento.producto,
+                )
+            ) {
+                continue;
+            }
+
+            let indexDia = infoDias.findIndex((x) => x.dia === descuento.dia);
+
+            if (indexDia < 0) {
+                infoDias.push({
+                    dia: descuento.dia,
+                    productos: [],
+                    orden: nombresDias.indexOf(descuento.dia.toLowerCase()),
+                });
+
+                indexDia = infoDias.length - 1;
+            }
+
+            let indexProducto = infoDias[indexDia].productos.findIndex(
+                (x) => x.nombre === descuento.producto,
+            );
+
+            if (indexProducto < 0) {
+                infoDias[indexDia].productos.push({
+                    nombre: descuento.producto,
+                    proveedor: descuento.proveedor,
+                    ofertas: [],
+                });
+
+                indexProducto = infoDias[indexDia].productos.length - 1;
+            }
+
+            let indexOferta = infoDias[indexDia].productos[
+                indexProducto
+            ].ofertas.findIndex((x) => x.tienda === descuento.tienda);
+
+            if (indexOferta < 0) {
+                infoDias[indexDia].productos[indexProducto].ofertas.push({
+                    tienda: descuento.tienda,
+                    descuento: descuento.descuento,
+                });
+            }
+        }
+
+        infoDias.sort((a, b) => {
+            if (a.orden > b.orden) {
+                return 1;
+            }
+
+            if (a.orden < b.orden) {
+                return -1;
+            }
+
+            return 0;
+        });
+
+        infoDias = [...infoDias];
+
+        console.log(infoDias);
+    }
 
     onMount(() => {
         fecha = capitalizeFirstLetter(
@@ -18,46 +136,20 @@
             }),
         );
 
-        beneficios = [
-            {
-                proveedor: "Banco de Chile",
-                producto: "Crédito Visa",
-                ofertas: [
-                    {
-                        tienda: "NAOKI",
-                        descuento: 40,
-                    },
-                ],
-            },
-            {
-                proveedor: "Banco Santander",
-                producto: "Crédito WM LIM",
-                ofertas: [
-                    {
-                        tienda: "NAOKI",
-                        descuento: 40,
-                    },
-                    {
-                        tienda: "RISHTEDAR",
-                        descuento: 40,
-                    },
-                ],
-            },
-            {
-                proveedor: "Banco Falabella",
-                producto: "Débito",
-                ofertas: [
-                    {
-                        tienda: "TIGRE BRAVO",
-                        descuento: 40,
-                    },
-                    {
-                        tienda: "MIRADOR GOURMET",
-                        descuento: 40,
-                    },
-                ],
-            },
-        ];
+        try {
+            const rawProductosUsuario =
+                localStorage.getItem(KEY_PRODUCTOS) ?? "[]";
+
+            productosUsuario = Producto.array().parse(
+                JSON.parse(rawProductosUsuario),
+            );
+        } catch (error) {
+            console.error("error al obtener productos guardados", error);
+        }
+
+        productosUsuario = [...productosUsuario];
+
+        filtrarDescuentos(data);
     });
 </script>
 
@@ -80,9 +172,24 @@
         </div>
     </div>
 
-    {#each beneficios as beneficio}
-        <VistaBeneficio {beneficio} />
-    {/each}
+    <div class="descuentos mt-4">
+        {#each infoDias as dia}
+            <div class="dia">
+                <div class="subtitulo">Septiembre: {dia.dia}</div>
+
+                {#each dia.productos as producto}
+                    <div>{producto.proveedor} - {producto.nombre}</div>
+
+                    {#each producto.ofertas as oferta}
+                        <div class="oferta">
+                            <span>{oferta.tienda}</span>
+                            <span>{oferta.descuento} %</span>
+                        </div>
+                    {/each}
+                {/each}
+            </div>
+        {/each}
+    </div>
 </section>
 
 <style>
@@ -105,5 +212,25 @@
 
     .button {
         background-color: var(--color2);
+    }
+
+    .dia {
+        background-color: var(--color2);
+        border-radius: 0.5rem;
+        margin-bottom: 0.7rem;
+        padding: 0.7rem;
+        font-weight: bold;
+        color: white;
+    }
+
+    .subtitulo {
+        font-size: small;
+        color: rgb(173, 173, 173);
+    }
+
+    .oferta {
+        color: rgb(120, 204, 85);
+        display: flex;
+        justify-content: space-between;
     }
 </style>
