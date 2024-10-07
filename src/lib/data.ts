8263eac6971273c36473c6c717537dab9d51b42b
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2024-10-06 22:47:05 -0300

    Ajuste.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -50,7 +50,14 @@ export function convertirDescuentos(filas: FilaDescuento[]): Descuento[] {
                         for (const segmento of segmentosTarjeta) {
                             for (const categoria of categoriasTarjeta) {
                                 for (const dia of dias) {
-                                    if (descuentos.find(x => x.producto.institucion === institucion && x.producto.marca === marcaTarjeta && x.producto.nombre === producto && x.producto.segmento === segmento && x.dia === dia && x.descuento === descuento) != null) {
+                                    if (descuentos.find(
+                                        x => x.producto.institucion === institucion &&
+                                            x.producto.marca === marcaTarjeta &&
+                                            x.producto.nombre === producto &&
+                                            x.producto.segmento === segmento &&
+                                            x.dia === dia &&
+                                            x.descuento === descuento) != null
+                                    ) {
                                         continue;
                                     }
 
